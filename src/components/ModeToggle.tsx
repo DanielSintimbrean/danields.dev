19d718b816c4dfcd9599887d25df51b47efc7157
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2024-03-02 21:52:11 +0100

    Fix footer and mode toggle icons

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -32,7 +32,7 @@ export function ModeToggle() {
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:hidden dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute hidden h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:block dark:rotate-0 dark:scale-100" />
+          <Moon className="hidden h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:block dark:rotate-0 dark:scale-100" />
           <span className="sr-only text-foreground">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
