9c47c16d88829a92d22f8463ec28431d34c6ebeb
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2024-03-02 23:32:26 +0100

    Add Astro robots.txt integration and update homepage content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
+import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    robotsTxt(),
+  ],
+  site: "https:danields.dev",
+  output: "static",
+  adapter: vercel({ webAnalytics: true }),
 });
