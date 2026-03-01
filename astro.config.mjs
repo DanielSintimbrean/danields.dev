1b730f8fedaf5cf41cc41c98f07b7ec7908656b5
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2026-03-01 16:23:23 +0100

    feat: :sparkles: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    robotsTxt(),
-  ],
+  integrations: [react(), robotsTxt()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https:danields.dev",
   output: "static",
   adapter: vercel({ webAnalytics: true }),
